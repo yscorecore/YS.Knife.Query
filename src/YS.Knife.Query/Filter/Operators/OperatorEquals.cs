ff9501b964ff1632dd4af715e1dd8c382cd5430d
pengbo.yang <obpt@sina.com>
2024-07-09 22:08:39 +0800

    add test cases between valuepath and const

--- src/YS.Knife.Query/Filter/Operators/OperatorEquals.cs
+++ src/YS.Knife.Query/Filter/Operators/OperatorEquals.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Linq.Expressions;
 using YS.Knife.Query.Expressions;
 
 namespace YS.Knife.Query.Filter.Operators
@@ -13,11 +11,98 @@ namespace YS.Knife.Query.Filter.Operators
 
         public override ValueExpressionDesc CreatePredicateExpression(OperatorExpressionContext context)
         {
+            var leftNode = context.Left;
+            var rightNode = context.Right;
+            if (leftNode.ValueType != rightNode.ValueType)
+            {
+                if (rightNode.IsConstant)
+                {
+                    if (CanConvertValue(rightNode.ValueType, leftNode.ValueType, out var converter))
+                    {
+                        rightNode = RebuildConstantValue(rightNode, leftNode.ValueType, converter);
+                    }
+                }
+                else if (leftNode.IsConstant)
+                {
+                    if (CanConvertValue(leftNode.ValueType, rightNode.ValueType, out var converter))
+                    {
+                        leftNode = RebuildConstantValue(leftNode, rightNode.ValueType, converter);
+                    }
+                }
+                else
+                {
+                    if (CanConvertExpression(rightNode.ValueType, leftNode.ValueType))
+                    {
+                        rightNode = ConvertExpression(rightNode, leftNode.ValueType);
+                    }
+                    else if (CanConvertExpression(leftNode.ValueType, rightNode.ValueType))
+                    {
+                        leftNode = ConvertExpression(leftNode, rightNode.ValueType);
+                    }
+                }
+            }
+            if (leftNode.ValueType != rightNode.ValueType)
+            {
+                throw new QueryExpressionBuildException($"can not convert type between '{leftNode.ValueType.FullName}' and '{rightNode.ValueType.FullName}'");
+            }
+
             return new ValueExpressionDesc
             {
                 ValueType = typeof(bool),
-                Expression = System.Linq.Expressions.Expression.Equal(context.Left.Expression, context.Right.Expression)
+                Expression = DoOperatorAction(leftNode.Expression, rightNode.Expression)
             };
+
+
+        }
+
+        protected ValueExpressionDesc RebuildConstantValue(ValueExpressionDesc from, Type targetType, TypeConverter converter)
+        {
+            var originalExpression = (ConstantExpression)from.Expression;
+            var targetValue = converter.ConvertTo(originalExpression.Value, targetType);
+            return new ValueExpressionDesc
+            {
+                IsConstant = true,
+                ValueType = targetType,
+                Expression = Expression.Constant(targetValue, targetType)
+            };
+        }
+        protected ValueExpressionDesc ConvertExpression(ValueExpressionDesc from, Type targetType)
+        {
+            var originalExpression = (ConstantExpression)from.Expression;
+            return new ValueExpressionDesc
+            {
+                IsConstant = true,
+                ValueType = targetType,
+                Expression = Expression.Constant(originalExpression.Value, targetType)
+            };
+        }
+        protected virtual Expression DoOperatorAction(Expression left, Expression right)
+        {
+            return Expression.Equal(left, right);
+        }
+
+        bool CanConvertValue(Type fromType, Type toType, out TypeConverter converter)
+        {
+            converter = System.ComponentModel.TypeDescriptor.GetConverter(fromType);
+            if (converter != null && converter.CanConvertTo(toType))
+            {
+                return true;
+            }
+            return false;
+        }
+        bool CanConvertExpression(Type fromType, Type toType)
+        {
+            if (Nullable.GetUnderlyingType(fromType) == toType)
+            { 
+                
+            }
+            return false;
+        }
+        enum ConvertKind
+        { 
+            Nullable,
+            ToString,
+            Convert
         }
     }
 }
--- test/YS.Knife.Query.IntegrationTest/FilterExtensionsTest.cs
+++ test/YS.Knife.Query.IntegrationTest/FilterExtensionsTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,7 @@ namespace YS.Knife.Query.IntegrationTest
     public class FilterExtensionsTest
     {
         [Theory]
-        [InlineData(1, 1)]
+        [MemberData(nameof(GetConstantEqualsData))]
         public void ConstantEqualsTest(object left, object right)
         {
             var leftValue = new ValueInfo
@@ -34,6 +35,90 @@ namespace YS.Knife.Query.IntegrationTest
             var source = new int[] { 1 };
             var target = source.AsQueryable().DoFilter(filter).ToArray();
             target.Should().BeEquivalentTo(source);
+        }
+
+        public static IEnumerable<object[]> GetConstantEqualsData()
+        {
+            return new List<object[]>
+            {
+                new object[]{ 1, 1},
+                new object[]{ 1, (short)1},
+                new object[]{ 1, (ushort)1},
+                new object[]{ 1, (uint)1},
+                new object[]{ 1, (ulong)1},
+                new object[]{ 1, 1L},
+                new object[]{ 1, 1.0},
+                new object[]{ 1, 1.0M},
+                new object[]{(short)1,1},
+                new object[]{(ushort)1,1},
+                new object[]{(uint)1, 1 },
+                new object[]{(ulong)1, 1 },
+                new object[]{1L,1},
+                new object[]{1.0, 1 },
+                new object[]{ new DateTime(2024,7,8),new DateTime(2024,7,8)},
+            };
+        }
+        [Theory]
+        [MemberData(nameof(GetValuePathAndConstantEqualsTestData))]
+        public void ValuePathAndConstantEqualsTest(Type itemType, object pathValue, object constantValue)
+        {
+            var leftValue = new ValueInfo
+            {
+                NavigatePaths = new List<ValuePath>
+                {
+                    new ValuePath{ Name=nameof(Entity1<string>.Val) }
+                }
+            };
+            var rightValue = new ValueInfo
+            {
+                IsConstant = true,
+                ConstantValue = constantValue
+            };
+            var filter = new FilterInfo
+            {
+                OpType = CombinSymbol.SingleItem,
+                Left = leftValue,
+                Right = rightValue,
+                Operator = Operator.Equals
+            };
+            var method = this.GetType().GetMethod(nameof(GetFiltedResult), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .MakeGenericMethod(itemType);
+            IList res = (IList)(method.Invoke(this, new object[] { pathValue, filter }));
+            res.Count.Should().Be(1);
+        }
+        List<Entity1<T>> GetFiltedResult<T>(T val, FilterInfo filterInfo)
+        {
+            return getTestSource<T>(val).DoFilter(filterInfo).ToList();
+        }
+        IQueryable<Entity1<T>> getTestSource<T>(T val)
+        {
+            return new List<Entity1<T>>
+            {
+                new Entity1<T>{ Val = val }
+            }.AsQueryable();
+        }
+        public static IEnumerable<object[]> GetValuePathAndConstantEqualsTestData()
+        {
+            return new List<object[]>
+            {
+                new object[]{ typeof(string),"abc","abc"},
+                new object[]{ typeof(string), "1" , 1},
+                new object[]{ typeof(int?), 1 , 1},
+                new object[]{ typeof(int?), null , null},
+                new object[]{ typeof(int), 1 , 1L},
+                new object[]{ typeof(int?), 1 , 1L},
+                new object[]{ typeof(int), 1 , 1.0},
+                new object[]{ typeof(int?), 1 , 1.0},
+                new object[]{ typeof(int), 1 , 1.0M},
+                new object[]{ typeof(int?), 1 , 1.0M},
+                new object[]{ typeof(int), 1 , "1"},
+                new object[]{ typeof(int?), 1 , "1"},
+
+            };
+        }
+        record Entity1<T>
+        {
+            public T Val { get; set; }
         }
     }
 }
